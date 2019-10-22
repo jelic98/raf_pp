ba64b56862ce38e61f95a8ab5b575773fc7046cd
Lazar Jelic <lazarjelic98@gmail.com>
2019-10-22 23:10:59 +0200

    Added explanations in comments

--- 01_interpreter/lexer.py
+++ 01_interpreter/lexer.py
@@ -21,20 +21,26 @@ class Lexer():
     def msg_error(self, msg):
         raise Exception(msg)
 
+    # parse funciton (only in input_mode = 2)
     def parse_function(self):
         name = ''
         space = False
 
         start_pos = self.pos
 
+        # get function name
         while(self.pos < len(self.text) and self.text[self.pos] != '('):
             if self.text[self.pos].isalpha():
+                # check if function name includes space
                 if space:
                     self.msg_error("Function name cannot include space")
                 name += self.text[self.pos]
+
+            # check if there is space in function name
             space = self.text[self.pos].isspace()
             self.pos += 1
         
+        # check if function definition (input_mode = 2) ot its usage (input_mode = 1,3) is correct
         if((self.pos < len(self.text) and self.text[self.pos] != '(') or self.pos >= len(self.text)):
             return None
 
@@ -45,20 +51,32 @@ class Lexer():
 
         self.skip_whitespace()
         
+        # get function parameters
         while(self.pos < len(self.text) and self.text[self.pos].isalnum()):
+            # create temporary string that will hold parameter names
             pname += self.text[self.pos]
             self.pos += 1
+            
+            # skip whitespace between commas and parameters
             self.skip_whitespace()
+            
+            # check if there is more parameters to parse
             if self.text[self.pos] in [',', ')']:
+                # add parameter to function parameter list 
                 params.append(pname)
+            
+                # reset current parameter name to parse next one
                 pname = ""
                 self.pos += 1
+            
+                # skip whitespace between parameters and commas
                 self.skip_whitespace()
          
 
         if self.text[self.pos - 1] != ')':
             elf.msg_error("Parameter list must end with parameter")
 
+        # create function (input_mode = 1, 2, 3)
         fun = {}
         fun["name"] = name
         fun["params"] = params
@@ -66,6 +84,8 @@ class Lexer():
 
         fun_call = self.text[start_pos:self.pos]
 
+        # this is tupple because we need reference to defined function
+        # as well as it concrete usage in input_mode = 1
         return (fun, fun_call)
 
     def get_next_token(self):
@@ -92,6 +112,9 @@ class Lexer():
             token = Token(LPAREN, '(')
         elif current_char == ')':
             token = Token(RPAREN, ')')
+        # since we are not using variables in input_mode = 1
+        # checking if current char is letter is okay
+        # for checking if next token is function
         elif current_char.isalpha():
             token =  Token(FUN, self.parse_function())
         else:
--- 01_interpreter/main.py
+++ 01_interpreter/main.py
@@ -2,22 +2,24 @@ from token import Token
 from type import *
 from lexer import Lexer
 
-# FUNCTION MAP
-# _def --> fun(a, b, c)
-# _def --> a + b + c
+# function map
+# --> def ; input_mode = 1
+# _def --> fun(a, b, c) ; input_mode = 2
+# _def --> a + b + c ; input_mode = 3
 # "fun" -> [
-#   "name" -> "fun"
-#   "params" -> ["a", "b", "c"],
-#   "expr" -> "a + b + c",
-#   "calls" -> [g, fun]
+#   "name" -> "fun" ; function name
+#   "params" -> ["a", "b", "c"] ; function parameters
+#   "expr" -> "a + b + c" ; replacement expression
+#   "calls" -> [fun1, fun2] ; child functions called from parent function
 # ]
 
-# input_mode = 1 ; normal mode
-# input_mode = 2 ; define mode signature
-# input_mode = 3 ; define mode body
+# current input mode (1, 2, 3)
+input_mode = 1
 
-input_mode = 2;
+# every defined function
 funs = {}
+
+# last defined function
 last_fun = None
 
 class Interpreter():
@@ -25,11 +27,12 @@ class Interpreter():
         self.lexer = lexer
         self.current_token = self.lexer.get_next_token()
 
-    def error(self, token_type):
-        raise Exception("Expected {} but found {}".format(token_type, self.current_token.token_type))
-
+    # print custom error message
     def msg_error(self, msg):
         raise Exception(msg)
+
+    def error(self, token_type):
+        msg_error("Expected {} but found {}".format(token_type, self.current_token.token_type))
 
     def eat(self, token_type):
         if self.current_token.token_type == token_type:
@@ -40,31 +43,41 @@ class Interpreter():
     def factor(self):
         token = self.current_token
         
+        # check if current token is function (only in input_mode = 1)
         if token.token_type == FUN:
+            # token value is tupple
+            # first element is function map
+            # second element is concrete usage of defined function
             fun = token.value[0]
+            fun_call = token.value[1]
             
+            # check if function is defined
             if not fun["name"] in funs:
                 msg_error("Function not defined")
             
+            # get defined function
             fun_defined = funs[fun["name"]]
             
+            # check parameter 
             if len(fun["params"]) != len(fun_defined["params"]):
                 self.msg_error("Calling function {} with {} instead of {} parameters".format(fun["name"], len(fun["params"]), len(fun_defined["params"])))
             
             pos = 0
+
+            # put replacement expression in parenthesis to isolate computation
             expr = "(" + fun_defined["expr"] + ")"
-            fun = token.value[0]
-            fun_call = token.value[1]
             
+            # replace defined parameters with concrete arguments
             for param in fun_defined["params"]:
                 expr = expr.replace(param, fun["params"][pos])
                 pos += 1
-            
+           
+            # shift cursor to adapt to replacement expression 
             self.lexer.pos = self.lexer.text.find(fun_call) + 1
             self.lexer.text = self.lexer.text.replace(fun_call, expr, 1)
 
+            # resolve expression in parenthesis
             self.current_token = Token(LPAREN, "(")
-
             return self.factor()
         if token.token_type == INTEGER:
             self.eat(INTEGER)
@@ -110,8 +123,10 @@ class Interpreter():
 
         return left        
 
+    # define function signature (only in input_mode = 2)
     def define(self):
         global funs, last_fun
+        
         token = self.current_token
         fun = token.value[0]
 
@@ -121,34 +136,64 @@ class Interpreter():
 
         funs[fun["name"]] = fun
         last_fun = fun
+        
         return token
     
-    def check_recursion(self, search, fun):
-        for call in funs[fun["name"]]["calls"]:
+    # checking infinite recursion
+    # by searching function name in bodies of its child functions
+    def check_recursion(self, search, fname):
+        # for every child function
+        for call in funs[fname]["calls"]:
+            # if child function has same name as function we are searching for
             if search["name"] == call["name"]:
+                # stop execution and print error
                 self.msg_error("Infinite recursion detected")
-            self.check_recursion(search, call)
+            # else check recursively for grand children
+            self.check_recursion(search, call["name"])
 
+    # define function body (only in input_mode = 3)
     def define_body(self, text):
         global funs, last_fun
+
+        # check if function signature is defined
         if last_fun is not None:
+            # get replacement expression
             last_fun["expr"] = text
-            pos = 0;
+            pos = 0
+        
+            # check if current function is calling another function
+            # that is not defined
+            # otherwise add child function to "calls" list of current function
+            # if function does not call another function this
+            # while loop will just increment pos to the end of text
             while pos < len(text):
+                # if current character is letter (beginning of function name)
                 if text[pos].isalpha():
+                    # create new lexer for parsing child function
                     new_lexer = Lexer(text, pos)
+                    
+                    # get child function token
                     call_tok = new_lexer.get_next_token()
+
+                    # if token has value (function is correctly parser)
                     if call_tok.value is not None:
                         call = call_tok.value[0]
+                        
+                        # check if token is actually function
                         if call_tok.token_type == FUN:
+
+                            # check if child function is defined
                             if call["name"] not in funs:
                                 self.msg_error("Function {} not defined".format(call["name"]))
 
+                        # append child function to children list of last defined function
                         last_fun["calls"].append(call)
                     pos = new_lexer.pos
                 else:
                     pos += 1
-            self.check_recursion(last_fun, last_fun)
+            
+            # check if defined function has infinite recursion
+            self.check_recursion(last_fun, last_fun["name"])
 
 def main():
     global input_mode;
@@ -168,7 +213,8 @@ def main():
 
         if text == 'exit':
             break
-         
+        
+        # input mode switching logic 1 -> 2 -> 3 -> 1
         if input_mode == 1:
             lexer = Lexer(text)
             intepreter = Interpreter(lexer)
