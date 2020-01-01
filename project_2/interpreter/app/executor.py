3e7cd27f1ad82c21fd96005f6ed689884c8b93ae
Lazar Jelic <lazarjelic98@gmail.com>
2020-01-01 04:24:00 +0100

    Executor working: example01, example02

--- project_2/interpreter/app/executor.py
+++ project_2/interpreter/app/executor.py
@@ -1,3 +1,13 @@
+from app.nodes import *
+
+vars = []
+
+class Var:
+    def __init__(self, type, name, value):
+        self.type = type
+        self.name = name
+        self.value = value
+
 class NodeVisitor(object):
         def visit(self, node):
                 method_name = 'visit_' + type(node).__name__
@@ -16,72 +26,98 @@ class Executor(NodeVisitor):
                         self.visit(cvor)
         
         def visit_Postojanje(self, node):
-                self.visit(node.tip)
-                self.visit(node.naziv)
+                tip = self.visit(node.tip)
+                naziv = self.visit(node.naziv)
+                vars.append(Var(tip, naziv, None))
 
         def visit_Dodela(self, node):
-                self.visit(node.izraz)
-                self.visit(node.varijabla)
+                izraz = self.visit(node.izraz)
+                varijabla = self.visit(node.varijabla)
+
+                for var in vars:
+                    if var.name == varijabla:
+                        var.value = izraz
+                        break
 
         def visit_Polje(self, node):
-                self.visit(node.tip)
-                self.visit(node.naziv)
+                tip = self.visit(node.tip)
+                naziv = self.visit(node.naziv)
 
         def visit_Rutina(self, node):
                 if node.tip is not None:
-                    self.visit(node.tip)
+                    tip = self.visit(node.tip)
 
-                self.visit(node.sadrzaj)
-                self.visit(node.naziv)
+                sadrzaj = self.visit(node.sadrzaj)
+                naziv = self.visit(node.naziv)
 
         def visit_Argumenti(self, node):
+                argumenti = []
                 for arg in node.argumenti:
                         if arg is not None:
-                                self.visit(arg)
+                                argumenti.append(self.visit(arg))
+                return argumenti
 
         def visit_RutinaPoziv(self, node):
-                self.visit(node.argumenti)
-                self.visit(node.naziv)
+                argumenti = self.visit(node.argumenti)
+                naziv = self.visit(node.naziv)
 
         def visit_UgradjenaRutinaPoziv(self, node):
-                self.visit(node.argumenti)
-                self.visit(node.naziv)
+                argumenti = self.visit(node.argumenti)
+                naziv = self.visit(node.naziv)
+                if naziv == "ucitaj":
+                    return input()
+                elif naziv == "ispisi":
+                    print(argumenti[0])
+                elif naziv == "spoji_strune":
+                    s = ""
+                    for arg in node.argumenti.argumenti:
+                        if isinstance(arg, Naziv):
+                            for var in vars:
+                                if var.name == arg.naziv:
+                                    s += str(var.value)
+                                    break
+                        else:
+                            s += str(self.visit(arg))
+                    return s
+                return None
 
         def visit_Vrati(self, node):
-                self.visit(node.izraz)
+                izraz = self.visit(node.izraz)
 
         def visit_PrekiniPonavljanje(self, node):
                 pass
         
         def visit_NaredbaUslov(self, node):
-                self.visit(node.pitanje)
-                self.visit(node.da)
+                pitanje = self.visit(node.pitanje)
 
-                if node.ne != None:
-                        self.visit(node.ne)
+                if pitanje:
+                    da = self.visit(node.da)
+                else:
+                    if node.ne != None:
+                        ne = self.visit(node.ne)
 
         def visit_NaredbaPonavljanje(self, node):
-                self.visit(node.pitanje)
-                self.visit(node.ponovi)
+                pitanje = self.visit(node.pitanje)
+                ponovi = self.visit(node.ponovi)
 
         def visit_CelinaCelina(self, node):
                 for cvor in node.cvorovi:
                         self.visit(cvor)
 
         def visit_CeoBroj(self, node):
-                pass
+                return node.broj
 
         def visit_Struna(self, node):
-                pass
+                return node.struna
 
         def visit_JesteNije(self, node):
-                pass
+                return node.jestenije
 
         def visit_TipPodatka(self, node):
-                pass
+                return node.tip
 
         def visit_Naziv(self, node):
-                pass
+                return node.naziv
 
         def visit_ElementNiza(self, node):
                 self.visit(node.naziv)
@@ -91,8 +127,42 @@ class Executor(NodeVisitor):
                                 self.visit(indeks)
 
         def visit_BinarnaOperacija(self, node):
-                self.visit(node.prvi)
-                self.visit(node.drugi)
+                prvi = self.visit(node.prvi)
+                drugi = self.visit(node.drugi)
+               
+                if isinstance(node.prvi, Naziv):
+                    for var in vars:
+                        if var.name == prvi:
+                            prvi = var.value
+                            break
+
+                if isinstance(node.drugi, Naziv):
+                    for var in vars:
+                        if var.name == drugi:
+                            drugi = var.value
+                            break
+
+                if node.simbol == '+':
+                    return float(prvi) + float(drugi)
+                elif node.simbol == '-':
+                    return float(prvi) - float(drugi)
+                elif node.simbol == '*':
+                    return float(prvi) * float(drugi)
+                elif node.simbol == '/':
+                    return float(prvi) / float(drugi)
+                elif node.simbol == '%':
+                    return int(prvi) % int(drugi)
+                elif node.simbol == '<':
+                    return float(prvi) < float(drugi)
+                elif node.simbol == '>':
+                    return float(prvi) > float(drugi)
+                elif node.simbol == '<=':
+                    return float(prvi) >= float(drugi)
+                elif node.simbol == '>=':
+                    return float(prvi) <= float(drugi)
+                elif node.simbol == '=':
+                    return float(prvi) == float(drugi)
+                return None
 
         def visit_UnarnaOperacija(self, node):
                 self.visit(node.prvi)
